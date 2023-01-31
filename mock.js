e75189ee57594993d28b4ff1a1ef6afe344efd7f
TECHB_JMJ\jmjju <jmjjun3>
2023-01-31 18:49:43 +0900

    mock 데이터 추가

---
+++ mock.js
@@ -0,0 +1,11 @@
+export const campaign = {
+  suggestion_campaign: [
+    {
+      imageUrl: "",
+      sns_names: ["블로그"],
+      title: "속초코다리냉면",
+      description: "매콤하고~ 톡! 쏘는 맛의 코다리 냉면",
+      
+    }
+  ]
+}
